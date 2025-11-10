e21b9c30f5da05978705f0cd0b8d7def45f522ca
MSCRATCH <me@pathologicalplay.com>
2025-11-10 18:11:56 +0100

    Add files via upload

---
+++ themes/backend_template/backend_footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer class="footer">
+<p><?php echo sanitize(name); ?> <?php echo sanitize(version); ?></p>
+<p>programmed by pathologicalplay</p>
+</footer>
+</div>
+</div>
+</body>
+</html>
---
+++ themes/backend_template/backend_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<title><?php echo sanitize(name); ?></title>
+<meta charset="utf-8">
+<meta name="robots" content="INDEX,FOLLOW">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="pathologicalplay">
+<meta name="revisit-after" content="2 days">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="themes/backend_template/backend.css" media="all" type="text/css">
+</head>
+<body>
+<div class="backend_template_row">
+<div class="backend_template_column_1">
+<div class="backend_template_nav">
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="index.php?section=user_management">Manage users</a></li>
+</ul>
+</div>
+</div>
+<div class="backend_template_column_2">
+<main>
